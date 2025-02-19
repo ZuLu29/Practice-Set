96a8750aced7a538accf0b620e6c8100841a9467
ZuLu29 <markneilconde850@gmail.com>
2025-02-20 00:31:53 +0800

    dsiplay process

--- PART 2/word-frequency-counter-main/process.php
+++ PART 2/word-frequency-counter-main/process.php
@@ -32,9 +32,32 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Word Frequency Counter - Results</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    
+    <h1>Word Frequency Counter - Results</h1>
+    <a href="index.html">Back to Input</a>
+    <br><br>
+    <?php if (!empty($wordFrequencies)): ?>
+        <table>
+            <thead>
+                <tr>
+                    <th>Word</th>
+                    <th>Frequency</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($wordFrequencies as $word => $frequency): ?>
+                    <tr>
+                        <td><?php echo htmlspecialchars($word); ?></td>
+                        <td><?php echo $frequency; ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php else: ?>
+        <p>No words to display.</p>
+    <?php endif; ?>
 </body>
 </html>

f91db732524d42bf4d9affd5dfc1436f6493889a
ZuLu29 <markneilconde850@gmail.com>
2025-02-20 00:13:09 +0800

    adding stop words

--- PART 2/word-frequency-counter-main/process.php
+++ PART 2/word-frequency-counter-main/process.php
@@ -1,4 +1,8 @@
 <?php
+
+$stopWords = ["the", "and", "in", "of", "to", "a", "is", "for", "on", "with", "as", "an", "at", "by", "that", 
+"would", "should", "can", "could", "may", "might", "must", "about", "above", "below", "into", "out", "same", "so", 
+"than", "too", "very", "s", "t", "can't", "don't", "shouldn't", "won't", "wouldn't"];
 $inputText = $_POST['text']; 
 $words = str_word_count(strtolower($inputText), 1);
 ?>

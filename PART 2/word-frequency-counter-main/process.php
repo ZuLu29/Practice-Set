c1257eb081d152994a04001a34a51262b65533a8
ZuLu29 <markneilconde850@gmail.com>
2025-02-20 00:14:26 +0800

    filter fucntion

--- PART 2/word-frequency-counter-main/process.php
+++ PART 2/word-frequency-counter-main/process.php
@@ -3,6 +3,14 @@
 $stopWords = ["the", "and", "in", "of", "to", "a", "is", "for", "on", "with", "as", "an", "at", "by", "that", 
 "would", "should", "can", "could", "may", "might", "must", "about", "above", "below", "into", "out", "same", "so", 
 "than", "too", "very", "s", "t", "can't", "don't", "shouldn't", "won't", "wouldn't"];
-$inputText = $_POST['text']; 
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $inputText = $_POST['text'];
+    $sortingOrder = $_POST['sort'];
+    $displayLimit = (int)$_POST['limit'];
+
+    $filteredWords = array_diff($words, $stopWords);
+}
+
 $words = str_word_count(strtolower($inputText), 1);
 ?>

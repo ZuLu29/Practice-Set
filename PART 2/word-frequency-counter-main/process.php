a1ae6688c105570e26456edc1d64555dfb40f00e
ZuLu29 <markneilconde850@gmail.com>
2025-02-20 00:15:38 +0800

    calculate word fucntion

--- PART 2/word-frequency-counter-main/process.php
+++ PART 2/word-frequency-counter-main/process.php
@@ -9,8 +9,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $sortingOrder = $_POST['sort'];
     $displayLimit = (int)$_POST['limit'];
 
+    $words = str_word_count(strtolower($inputText), 1);
+
     $filteredWords = array_diff($words, $stopWords);
+    
+    $wordFrequencies = array_count_values($filteredWords);
+
+
 }
 
-$words = str_word_count(strtolower($inputText), 1);
+
 ?>
